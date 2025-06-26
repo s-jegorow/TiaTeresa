e673f6bd59101c86a023406eb8f512fe17293d85
bunecarlos <teilnahme@sebastianjegorow.de>
2025-06-26 11:47:39 +0200

    startseite update 1 - noch mit fixer viewbag

--- TiaTeresa/Controllers/HomeController.cs
+++ TiaTeresa/Controllers/HomeController.cs
@@ -15,7 +15,13 @@ namespace TiaTeresa.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.Sprichwort = "No hay mal que por bien no venga.";
+            ViewBag.SprichwortÜbersetzung = "Kein Unglück, das nicht auch etwas Gutes bringt.";
+
+            ViewBag.Vokabel = "el paraguas";
+            ViewBag.VokabelÜbersetzung = "der Regenschirm";
             return View();
+        
         }
 
         
